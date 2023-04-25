76400da54412ac04e5294e90afaf58dd2fa4b41e
aidan <aidan@Aidan.myguest.virtualbox.org>
2023-04-25 12:22:07 -0400

    fixing

--- lua/hello/rustsyscall.lua
+++ lua/hello/rustsyscall.lua
@@ -1,38 +1,33 @@
---put lua stuff in here to:
---call a bash script maybe? that calls rust asm func with correct params and outputs into a specific window
---things i need to learn to do:
---1. how to get function name we want to display, and pass that into command line args for rust asm
---2. how to display output in a window in neovim
---lfg i think i have a way to do this now, just need to learn how to do this stuff
-
 local api = vim.api
 local buf, win
 
 local function open_window()
-    buf = api.nvim_create_buf(false, true)
+  buf = api.nvim_create_buf(false, true) -- create new emtpy buffer
 
-    api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
 
-    local width = api.nvim_get_option("columns")
-    local height = api.nvim_get_option("lines")
+  -- get dimensions
+  local width = api.nvim_get_option("columns")
+  local height = api.nvim_get_option("lines")
 
+  -- calculate our floating window size
+  local win_height = math.ceil(height * 0.8 - 4)
+  local win_width = math.ceil(width * 0.8)
 
-    local win_height = math.ceil(height * 0.8 - 4)
-    local win_width = math.ceil(width * 0.8)
+  -- and its starting position
+  local row = math.ceil((height - win_height) / 2 - 1)
+  local col = math.ceil((width - win_width) / 2)
 
-    local row = math.ceil((height - win_height) / 2 -1)
-    local col = math.ceil((width - win_width) / 2)
+  -- set some options
+  local opts = {
+    style = "minimal",
+    relative = "editor",
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col
+  }
 
-    local opts = {
-        style = "minimal", 
-        relative = "editor",
-        width = win_width,
-        height = win_height,
-        row = row,
-        col = col
-    }
-
-
-    win = api.nvim_open_win(buf, true, opts)
-
+  -- and finally create it with buffer attached
+  win = api.nvim_open_win(buf, true, opts)
 end
