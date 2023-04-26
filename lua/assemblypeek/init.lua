cf8ed20bf8011a4de795b3d9cbbb218a11f5ac7c
aidan <aidan@Aidan.myguest.virtualbox.org>
2023-04-25 20:05:13 -0400

    adding user input

--- lua/assemblypeek/init.lua
+++ lua/assemblypeek/init.lua
@@ -1,9 +1,8 @@
 
 local function show_assembly()
-    local file = vim.fn.expand("%:p")
-    print("my file is" .. file)
+    input = vim.ui.input({prompt = "enter function you want to inspect as assembly: "})
     vim.cmd("vsplit | terminal")
-    local command = ':call jobsend(b:terminal_job_id, "cargo asm --asm-style=intel fizzbuzz::main \\n")'
+    local command = ':call jobsend(b:terminal_job_id, "cargo asm --asm-style=intel fizzbuzz::.." input .. "\\n")'
     vim.cmd(command)
 end
 
